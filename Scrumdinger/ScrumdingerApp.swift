c1de3323285cc7cf398fdbf1ff78e529ca063c16
coreypeoples <cpeoplesrfp@gmail.com>
2022-04-22 23:59:07 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Corey Peoples on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
